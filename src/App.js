0c3ac91689058f10541d303fd3b6f9536fbad4f9
Dexter <65760336+chanphiromsok@users.noreply.github.com>
2021-06-03 22:02:17 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import "./App.css";
 import React from "react";
-import {CardList} from "./components/card-list/card-list.components";
+import { CardList } from "./components/card-list/card-list.components";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            monsters: [],
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
 
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((users) => this.setState({monsters: users}));
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <CardList>
-                    {this.state.monsters.map((monster) => (
-                        <h1 key={monster.id}>{monster.name}</h1>
-                    ))}
-                </CardList>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/card-list/card-list.components.jsx
+++ src/components/card-list/card-list.components.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./card-list-style.css";
 
-export const CardList = ({monsters}) =>{
-    console.log(monsters);
-    return(
-        <div>helo</div>
-    )
-}
+export const CardList = ({ monsters }) => {
+  console.log(monsters);
+  return (
+    <div>{monsters && monsters.map((mn) => <h1>Name : {mn.name}</h1>)}</div>
+  );
+};
